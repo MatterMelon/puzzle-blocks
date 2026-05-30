bc456d8cc4cc984562134e39f0f39662b8e11281
Andrey Popov <dil2232@gmail..com>
2026-05-30 13:16:10 +0500

    fix: made correct programm exit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pygame as pg
+import sys
 
+import pygame as pg
 from puzzle_block import PuzzleBlock
 
 pg.init()
@@ -11,9 +12,6 @@ pg.display.set_caption("Andy's game!")
 font = pg.font.SysFont('Arial', 24)
 clock = pg.time.Clock()
 
-# text_surface = font.render("Text", True, pg.Color(255, 255, 255))
-# text_rect = text_surface.get_rect(center = rect_obj.center)
-
 block_1 = PuzzleBlock(100, 100, 200, 100, (0, 0, 0))
 block_2 = PuzzleBlock(100, 300, 200, 100, (0, 0, 0))
 blocks = [block_1, block_2]
@@ -23,12 +21,12 @@ while True:
     for e in pg.event.get():
         if e.type == pg.QUIT:
             pg.quit()
+            sys.exit()
+            
         for block in blocks:
             block.handle_event(e)
         
     screen.fill(pg.Color(255,255,255))
-    # pg.draw.rect(screen, pg.Color(0, 0, 0), rect_obj, border_radius=15)
-    # screen.blit(text_surface, text_rect)
     for block in blocks:
         block.draw(screen)
     pg.display.flip()
