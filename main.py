69f2f0630c3223110939b9c5cfabd1db47ede362
Andrey Popov <dil2232@gmail..com>
2026-05-30 15:25:40 +0500

    refactor: reset project, added pytest and pygame-gui

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
 import sys
 
 import pygame as pg
-from puzzle_block import PuzzleBlock
 
 pg.init()
 pg.font.init()
 
 WINDOW_SIZE = (800, 600)
 screen = pg.display.set_mode(WINDOW_SIZE)
-pg.display.set_caption("Andy's game!")
+pg.display.set_caption("PuzzleBlocks")
 font = pg.font.SysFont('Arial', 24)
 clock = pg.time.Clock()
-
-block_1 = PuzzleBlock(100, 100, 200, 100, (0, 0, 0))
-block_2 = PuzzleBlock(100, 300, 200, 100, (0, 0, 0))
-blocks = [block_1, block_2]
-
 
 while True:
     for e in pg.event.get():
@@ -23,11 +17,6 @@ while True:
             pg.quit()
             sys.exit()
             
-        for block in blocks:
-            block.handle_event(e)
-        
     screen.fill(pg.Color(255,255,255))
-    for block in blocks:
-        block.draw(screen)
     pg.display.flip()
     clock.tick(60)
