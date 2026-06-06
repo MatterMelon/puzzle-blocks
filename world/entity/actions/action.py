4a81846ddacec43cb300fc0a5df3f8622608a3e1
Andrey Popov <dil2232@gmail..com>
2026-06-06 15:56:55 +0500

    refactor: incapsulation of action

--- world/entity/actions/action.py
+++ world/entity/actions/action.py
@@ -2,4 +2,4 @@ from abc import ABC
 
 
 class Action(ABC):
-    ...
+    pass
--- world/entity/entity.py
+++ world/entity/entity.py
@@ -2,6 +2,7 @@ import pygame as pg
 from pygame import Event
 from pygame.sprite import Sprite
 
+from world.entity.actions import action
 from world.entity.actions.action import Action
 
 
@@ -10,11 +11,19 @@ class Entity(Sprite):
         super().__init__(*groups)
         self.image: pg.Surface = pg.Surface((0, 0))
         self.rect: pg.Rect = self.image.get_rect()
-        self.action: Action | None = None
+        self._action: Action | None = None
+
+    @property
+    def action(self) -> Action | None:
+        return self._action
+
+    @action.setter
+    def action(self, action: Action) -> None:
+        self._action = action
     
     def get_action(self) -> Action | None:
-        action = self.action
-        self.action = None
+        action = self._action
+        self._action = None
         return action
 
     def move_to(self, dx: int, dy: int) -> None:
