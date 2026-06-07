ff34f6737d11ed9c7a56fafc50ba4065aa53e93e
Andrey Popov <dil2232@gmail..com>
2026-06-07 15:27:58 +0500

    feat: added scene_manager, moved part of game to game_scene

--- core/game.py
+++ core/game.py
@@ -3,13 +3,8 @@ import sys
 import pygame as pg
 
 from config.game_config import GameConfig
-from controllers.keyboard_controller import KeyboardController
-from world.entity.player import Player
-from world.level.exceptions import LevelError
-from world.level.level_data import LevelData
-from world.level.level_factory import LevelFactory
-from world.level.level_loader.json_level_loader import JsonLevelLoader
-from world.level.level_loader.level_loader import LevelLoader
+from scenes.game_scene import GameScene
+from scenes.scene_manager import SceneManager
 
 
 class Game:
@@ -22,38 +17,22 @@ class Game:
         pg.font.init()
         self._font = pg.font.SysFont(GameConfig.FONT_FAMILY, GameConfig.FONT_SIZE)
 
-        # TODO: Переписать LevelLoader на level_loader.load('path')
-        self._level_loader: LevelLoader = JsonLevelLoader('./data/levels/level_02.json')
-        self._level_data: LevelData | None = None
-        try:
-            self._level_data = self._level_loader.load()
-        except LevelError:
-            pass
-
-        self._controller = KeyboardController()
-        self._player = Player(16, 16)
-
-        self._level = LevelFactory().build_level(self._level_data)
-        self._level.add_entity(self._player)
-
+        self._scene_manager = SceneManager([GameScene()])
 
     def _process_events(self):
         for e in pg.event.get():
             if e.type == pg.QUIT:
                 pg.quit()
                 sys.exit()
-            action = self._controller.get_action(e)
-            if action:
-                self._player.action = action
-            self._level.handle_event(e)
+            self._scene_manager.process_scene(e)
 
     def update(self):
         self._clock.tick(60)
-        self._level.update()
+        self._scene_manager.update_scene()
 
     def render(self):
         self._screen.fill(pg.Color(255, 255, 255))
-        self._level.draw(self._screen)
+        self._scene_manager.render_scene(self._screen)
         pg.display.flip()
 
     def start(self):
